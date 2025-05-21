928c91be11da25e80ae16c5bbc6ba487a440cad5
itc-n25017 <n25017@std.it-college.ac.jp>
2025-05-21 07:38:00 +0000

    P50-P68 第四章演習　終了

---
+++ chapter04/ex4-02.py
@@ -0,0 +1,9 @@
+def f(x):
+  return x + 1
+
+z = f(4)
+if z == 5:
+    print("zis5")
+else:
+    print("z is not 5")
+
---
+++ chapter04/ex4-08.py
@@ -0,0 +1,7 @@
+age = input("enter your age:")
+int_age = int(age)
+if int_age < 21:
+    print("you are young!")
+else:
+    print("Wow, you are old!")
+
