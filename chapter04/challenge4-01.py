fc17be7a84e235a51b8bdbb388b7f16e477e10cf
itc-n25017 <n25017@std.it-college.ac.jp>
2025-05-22 06:41:18 +0000

    第４章　チャレンジ問題終了

---
+++ chapter04/challenge4-01.py
@@ -0,0 +1,6 @@
+def nunber(x):
+    return x ** 2
+
+result=nunber(int(input("数字を入力")))
+print(result)
+
---
+++ chapter04/challenge4-06.py
@@ -0,0 +1,40 @@
+def nunber(x):
+    """return x ** 2
+
+result=nunber(int(input("数字を入力")))"""
+print(result)
+
+x="mozi"
+
+def f():
+   """ print(x)
+
+f()"""
+
+def add_mult(a,b,c,x=100,z=1000):
+    """return a+b+c*x*z
+
+result = add_mult(10,15,25)"""
+print(result)
+
+def divide(x):
+    """return x / 2"""
+
+def multiply(x):
+    """return x * 4
+
+y = divide(4)
+z = multiply(y)"""
+
+print(z)
+
+try:
+    """a=input("数字入力")
+    b=input("数字を入力")
+    a=int(a)
+    b=int(b)
+    print(float(a/b))
+except(ZeroDivisionError,ValueError):"""
+    print("Invalid input")
+
+
